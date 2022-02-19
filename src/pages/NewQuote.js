56b4cbd8cf6ea75e44ca4cd7d30a1c69a293a0af
Said-Aabilla <said160aabilla@gmail.com>
2022-02-19 23:00:35 +0100

    programatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,18 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useNavigate } from "react-router-dom";
 
 const NewQuote = () => {
+  const navigate = useNavigate();
+
   const addQuoteHandler = (quote) => {
     console.log(quote);
+
+    navigate("/quotes"); // add this 2nd param { state } to navigate whilst keep state
+    
+    // replace the current location instead of push a new one onto the history stack
+    //navigate('/quotes', { replace: true })
   };
+
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
